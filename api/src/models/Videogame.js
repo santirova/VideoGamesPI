13bbbb654511870b09294fc66ae47abedd82180d
santilagos <sntlgs@gmail.com>
2023-05-09 12:02:01 -0300

    modelos creados

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type:DataTypes.UUID,
+      primaryKey:true,
+      defaultValue:DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    plataforms:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    image:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    },
+    relaseDate:{
+       type:DataTypes.DATE,
+       allowNull:false
+    },
+    rating:{
+      type:DataTypes.DECIMAL,
+      allowNull:false
+    }
   });
 };
