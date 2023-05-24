04d402ee3be10453b1d51fed07317a5567d7d90c
santilagos <sntlgs@gmail.com>
2023-05-24 16:41:41 -0300

    estilos al form y algunos detalles pulidos

--- client/src/views/FormPage/FormPage.jsx
+++ client/src/views/FormPage/FormPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import  Form  from '../../Components/Form/Form'
+import style from './FormPage.module.css'
 
 const FormPage = () => {
   return (
-    <Form/>
+    <div className={style.container}>
+      <Form/>
+    </div>
   )
 }
 
