8fdfe14b966af92f606bfb7ae29f600ce19f66c5
santilagos <sntlgs@gmail.com>
2024-03-15 18:21:49 -0300

    edited colors and font in landing page

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import style from './LandingPage.module.css';
 const LandingPage = () => {
   return (
     <div className={style.landingPage}>
-      <h1 className={style.title}>VideoGames PI</h1>
+      <h1 className={style.title}>GAME LAB</h1>
       <Link to="/home">
         <button className={style.button}>Let's Explore</button>
       </Link>
